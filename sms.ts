03b6319863e229a157d5e574e35f970552bac712
JAKHONGIR <52883946+djokk@users.noreply.github.com>
2025-06-17 13:23:59 +0500

    Add files via upload

---
+++ sms.ts
@@ -0,0 +1,64 @@
+import { defineStore } from 'pinia'
+import { reactive } from 'vue'
+
+interface User {
+	id: number
+	name: string
+	image: string
+	state: PersonState
+}
+
+interface Sms {
+	id: number
+	text: string
+	userId: number
+	image?: string
+	time: Date
+}
+
+interface Info {
+	user: number
+	state: string
+}
+
+export enum PersonState {
+	online = 'online',
+	offline = 'offline',
+	write = 'write',
+}
+
+export const useSmsStore = defineStore('messages', () => {
+	const users = reactive<User[]>([
+		{
+			id: 1,
+			name: 'Александр',
+			image: '/src/assets/img/1.png',
+			state: PersonState.online,
+		},
+		{
+			id: 2,
+			name: 'Евгений',
+			image: '/src/assets/img/2.png',
+			state: PersonState.online,
+		},
+	])
+	const messages = reactive<Sms[]>([
+		{
+			id: 0,
+			text: 'string',
+			userId: 1,
+			time: new Date(),
+		},
+	])
+
+	function resetState(info: Info) {
+		const user = users.find(user => user.id == info.id)
+		user.state = info.state
+	}
+
+	function addMessage(info: Sms) {
+		messages.push(info)
+	}
+
+	return { messages, resetState, addMessage, users, PersonState }
+})
